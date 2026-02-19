ae65aac2cecb07070afb5d82949ce478de9e9ee1
Augusto Vinicius <augustoviniciud@gmail.com>
2026-02-19 19:15:10 -0300

    fix ov_eval for ROS2

--- ov_eval/cmake/ROS2.cmake
+++ ov_eval/cmake/ROS2.cmake
@@ -1,132 +1,82 @@
-cmake_minimum_required(VERSION 3.3)
+cmake_minimum_required(VERSION 3.5)
+project(ov_eval)
 
-# Find ROS build system
 find_package(ament_cmake REQUIRED)
 find_package(rclcpp REQUIRED)
+find_package(geometry_msgs REQUIRED)
+find_package(nav_msgs REQUIRED)
 find_package(ov_core REQUIRED)
+find_package(Boost REQUIRED COMPONENTS filesystem system)
 
-# Describe ROS project
-option(ENABLE_ROS "Enable or disable building with ROS (if it is found)" ON)
-if (NOT ENABLE_ROS)
-    message(FATAL_ERROR "Build with ROS1.cmake if you don't have ROS.")
-endif ()
-add_definitions(-DROS_AVAILABLE=2)
+add_definitions(-DROS2)
 
-# Include our header files
+option(ENABLE_ROS "Enable or disable building with ROS" ON)
+
 include_directories(
-        src
-        ${EIGEN3_INCLUDE_DIR}
-        ${Boost_INCLUDE_DIRS}
-        ${PYTHON_INCLUDE_DIRS}
+    src
+    ${EIGEN3_INCLUDE_DIR}
+    ${Boost_INCLUDE_DIRS}
 )
-
-# Set link libraries used by all binaries
-list(APPEND thirdparty_libraries
-        ${Boost_LIBRARIES}
-)
-
-##################################################
-# Make the shared library
-##################################################
 
 list(APPEND LIBRARY_SOURCES
-        src/dummy.cpp
-        src/alignment/AlignTrajectory.cpp
-        src/alignment/AlignUtils.cpp
-        src/calc/ResultTrajectory.cpp
-        src/calc/ResultSimulation.cpp
-        src/utils/Loader.cpp
+    src/dummy.cpp
+    src/alignment/AlignTrajectory.cpp
+    src/alignment/AlignUtils.cpp
+    src/calc/ResultTrajectory.cpp
+    src/calc/ResultSimulation.cpp
+    src/utils/Loader.cpp
 )
-file(GLOB_RECURSE LIBRARY_HEADERS "src/*.h")
-add_library(ov_eval_lib SHARED ${LIBRARY_SOURCES} ${LIBRARY_HEADERS})
-ament_target_dependencies(ov_eval_lib rclcpp ov_core)
-target_link_libraries(ov_eval_lib ${thirdparty_libraries})
-target_include_directories(ov_eval_lib PUBLIC src/)
+
+add_library(ov_eval_lib SHARED ${LIBRARY_SOURCES})
+target_compile_definitions(ov_eval_lib PUBLIC ROS2)
+ament_target_dependencies(ov_eval_lib rclcpp ov_core geometry_msgs nav_msgs)
+target_link_libraries(ov_eval_lib ${Boost_LIBRARIES})
+
 install(TARGETS ov_eval_lib
-        LIBRARY DESTINATION lib
-        RUNTIME DESTINATION bin
-        PUBLIC_HEADER DESTINATION include
+    ARCHIVE DESTINATION lib
+    LIBRARY DESTINATION lib
+    RUNTIME DESTINATION bin
 )
+
 install(DIRECTORY src/
-        DESTINATION include
-        FILES_MATCHING PATTERN "*.h" PATTERN "*.hpp"
+    DESTINATION include/${PROJECT_NAME}
+    FILES_MATCHING PATTERN "*.h" PATTERN "*.hpp"
 )
+
+macro(ov_add_executable name source)
+    add_executable(${name} ${source})
+    target_compile_definitions(${name} PRIVATE ROS2)
+    ament_target_dependencies(${name} rclcpp ov_core geometry_msgs nav_msgs)
+    target_link_libraries(${name} ov_eval_lib)
+    install(TARGETS ${name} DESTINATION lib/${PROJECT_NAME})
+endmacro()
+
+if (ENABLE_ROS)
+    ov_add_executable(pose_to_file src/pose_to_file.cpp)
+    ov_add_executable(live_align_trajectory src/live_align_trajectory.cpp)
+endif()
+
+ov_add_executable(format_converter src/format_converter.cpp)
+ov_add_executable(error_comparison src/error_comparison.cpp)
+ov_add_executable(error_dataset src/error_dataset.cpp)
+ov_add_executable(error_singlerun src/error_singlerun.cpp)
+ov_add_executable(error_simulation src/error_simulation.cpp)
+ov_add_executable(timing_comparison src/timing_comparison.cpp)
+ov_add_executable(timing_flamegraph src/timing_flamegraph.cpp)
+ov_add_executable(timing_histogram src/timing_histogram.cpp)
+ov_add_executable(timing_percentages src/timing_percentages.cpp)
+ov_add_executable(plot_trajectories src/plot_trajectories.cpp)
+
+install(PROGRAMS
+    python/pid_ros.py
+    python/pid_sys.py
+    DESTINATION lib/${PROJECT_NAME}
+)
+
+install(DIRECTORY launch
+    DESTINATION share/${PROJECT_NAME}
+)
+
 ament_export_include_directories(include)
 ament_export_libraries(ov_eval_lib)
-
-##################################################
-# Make binary files!
-##################################################
-
-# TODO: UPGRADE THIS TO ROS2 AS ANOTHER FILE!!
-#if (catkin_FOUND AND ENABLE_ROS)
-#    add_executable(pose_to_file src/pose_to_file.cpp)
-#    target_link_libraries(pose_to_file ov_eval_lib ${thirdparty_libraries})
-#    add_executable(live_align_trajectory src/live_align_trajectory.cpp)
-#    target_link_libraries(live_align_trajectory ov_eval_lib ${thirdparty_libraries})
-#endif ()
-
-add_executable(format_converter src/format_converter.cpp)
-ament_target_dependencies(format_converter rclcpp ov_core)
-target_link_libraries(format_converter ov_eval_lib ${thirdparty_libraries})
-install(TARGETS format_converter DESTINATION lib/${PROJECT_NAME})
-
-add_executable(error_comparison src/error_comparison.cpp)
-ament_target_dependencies(error_comparison rclcpp ov_core)
-target_link_libraries(error_comparison ov_eval_lib ${thirdparty_libraries})
-install(TARGETS error_comparison DESTINATION lib/${PROJECT_NAME})
-
-add_executable(error_dataset src/error_dataset.cpp)
-ament_target_dependencies(error_dataset rclcpp ov_core)
-target_link_libraries(error_dataset ov_eval_lib ${thirdparty_libraries})
-install(TARGETS error_dataset DESTINATION lib/${PROJECT_NAME})
-
-add_executable(error_singlerun src/error_singlerun.cpp)
-ament_target_dependencies(error_singlerun rclcpp ov_core)
-target_link_libraries(error_singlerun ov_eval_lib ${thirdparty_libraries})
-install(TARGETS error_singlerun DESTINATION lib/${PROJECT_NAME})
-
-add_executable(error_simulation src/error_simulation.cpp)
-ament_target_dependencies(error_simulation rclcpp ov_core)
-target_link_libraries(error_simulation ov_eval_lib ${thirdparty_libraries})
-install(TARGETS error_simulation DESTINATION lib/${PROJECT_NAME})
-
-add_executable(timing_comparison src/timing_comparison.cpp)
-ament_target_dependencies(timing_comparison rclcpp ov_core)
-target_link_libraries(timing_comparison ov_eval_lib ${thirdparty_libraries})
-install(TARGETS timing_comparison DESTINATION lib/${PROJECT_NAME})
-
-add_executable(timing_flamegraph src/timing_flamegraph.cpp)
-ament_target_dependencies(timing_flamegraph rclcpp ov_core)
-target_link_libraries(timing_flamegraph ov_eval_lib ${thirdparty_libraries})
-install(TARGETS timing_flamegraph DESTINATION lib/${PROJECT_NAME})
-
-add_executable(timing_histogram src/timing_histogram.cpp)
-ament_target_dependencies(timing_histogram rclcpp ov_core)
-target_link_libraries(timing_histogram ov_eval_lib ${thirdparty_libraries})
-install(TARGETS timing_histogram DESTINATION lib/${PROJECT_NAME})
-
-add_executable(timing_percentages src/timing_percentages.cpp)
-ament_target_dependencies(timing_percentages rclcpp ov_core)
-target_link_libraries(timing_percentages ov_eval_lib ${thirdparty_libraries})
-install(TARGETS timing_percentages DESTINATION lib/${PROJECT_NAME})
-
-add_executable(plot_trajectories src/plot_trajectories.cpp)
-ament_target_dependencies(plot_trajectories rclcpp ov_core)
-target_link_libraries(plot_trajectories ov_eval_lib ${thirdparty_libraries})
-install(TARGETS plot_trajectories DESTINATION lib/${PROJECT_NAME})
-
-
-##################################################
-# Python scripts!
-##################################################
-
-# TODO: UPGRADE THIS TO ROS2 AS ANOTHER FILE!!
-#if (catkin_FOUND AND ENABLE_ROS)
-#    catkin_install_python(PROGRAMS python/pid_ros.py DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION})
-#    catkin_install_python(PROGRAMS python/pid_sys.py DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION})
-#endif ()
-
-
-# finally define this as the package
 ament_package()
--- ov_eval/src/utils/Recorder.h
+++ ov_eval/src/utils/Recorder.h
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.  See <https://www.gnu.org/licenses/>.
  */
 
 #ifndef OV_EVAL_RECORDER_H
@@ -29,50 +29,57 @@
 #include <Eigen/Eigen>
 #include <boost/filesystem.hpp>
 
+#ifdef ROS2
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#else
+#include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <nav_msgs/Odometry.h>
-#include <ros/ros.h>
+#endif
 
 namespace ov_eval {
 
-/**
- * @brief This class takes in published poses and writes them to file.
- *
- * Original code is based on this modified [posemsg_to_file](https://github.com/rpng/posemsg_to_file/).
- * Output is in a text file that is space deliminated and can be read by all scripts.
- * If we have a covariance then we also save the upper triangular part to file so we can calculate NEES values.
- */
 class Recorder {
 
 public:
-  /**
-   * @brief Default constructor that will open the specified file on disk.
-   * If the output directory does not exists this will also create the directory path to this file.
-   * @param filename Desired file we want to "record" into
-   */
+
   Recorder(std::string filename) {
-    // Create folder path to this location if not exists
     boost::filesystem::path dir(filename.c_str());
     if (boost::filesystem::create_directories(dir.parent_path())) {
+#ifdef ROS2
+      std::cout << "[INFO] Created folder path to output file." << std::endl;
+      std::cout << "[INFO] Path: " << dir.parent_path().string() << std::endl;
+#else
       ROS_INFO("Created folder path to output file.");
       ROS_INFO("Path: %s", dir.parent_path().c_str());
+#endif
     }
-    // If it exists, then delete it
     if (boost::filesystem::exists(filename)) {
+#ifdef ROS2
+      std::cout << "[WARN] Output file exists, deleting old file...." << std::endl;
+#else
       ROS_WARN("Output file exists, deleting old file....");
+#endif
       boost::filesystem::remove(filename);
     }
-    // Open this file we want to write to
     outfile.open(filename.c_str());
     if (outfile.fail()) {
+#ifdef ROS2
+      std::cerr << "[ERROR] Unable to open output file!!" << std::endl;
+      std::cerr << "[ERROR] Path: " << filename << std::endl;
+#else
       ROS_ERROR("Unable to open output file!!");
       ROS_ERROR("Path: %s", filename.c_str());
+#endif
       std::exit(EXIT_FAILURE);
     }
     outfile << "# timestamp(s) tx ty tz qx qy qz qw Pr11 Pr12 Pr13 Pr22 Pr23 Pr33 Pt11 Pt12 Pt13 Pt22 Pt23 Pt33" << std::endl;
-    // Set initial state values
     timestamp = -1;
     q_ItoG << 0, 0, 0, 1;
     p_IinG = Eigen::Vector3d::Zero();
@@ -81,14 +88,49 @@ public:
     has_covariance = false;
   }
 
-  /**
-   * @brief Callback for nav_msgs::Odometry message types.
-   *
-   * Note that covariance is in the order (x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis).
-   * http://docs.ros.org/api/geometry_msgs/html/msg/PoseWithCovariance.html
-   *
-   * @param msg New message
-   */
+#ifdef ROS2
+  void callback_odometry(const nav_msgs::msg::Odometry::SharedPtr msg) {
+    timestamp = rclcpp::Time(msg->header.stamp).seconds();
+    q_ItoG << msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w;
+    p_IinG << msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z;
+    cov_pos << msg->pose.covariance.at(0), msg->pose.covariance.at(1), msg->pose.covariance.at(2), msg->pose.covariance.at(6),
+        msg->pose.covariance.at(7), msg->pose.covariance.at(8), msg->pose.covariance.at(12), msg->pose.covariance.at(13),
+        msg->pose.covariance.at(14);
+    cov_rot << msg->pose.covariance.at(21), msg->pose.covariance.at(22), msg->pose.covariance.at(23), msg->pose.covariance.at(27),
+        msg->pose.covariance.at(28), msg->pose.covariance.at(29), msg->pose.covariance.at(33), msg->pose.covariance.at(34),
+        msg->pose.covariance.at(35);
+    has_covariance = true;
+    write();
+  }
+
+  void callback_pose(const geometry_msgs::msg::PoseStamped::SharedPtr msg) {
+    timestamp = rclcpp::Time(msg->header.stamp).seconds();
+    q_ItoG << msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z, msg->pose.orientation.w;
+    p_IinG << msg->pose.position.x, msg->pose.position.y, msg->pose.position.z;
+    write();
+  }
+
+  void callback_posecovariance(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg) {
+    timestamp = rclcpp::Time(msg->header.stamp).seconds();
+    q_ItoG << msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w;
+    p_IinG << msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z;
+    cov_pos << msg->pose.covariance.at(0), msg->pose.covariance.at(1), msg->pose.covariance.at(2), msg->pose.covariance.at(6),
+        msg->pose.covariance.at(7), msg->pose.covariance.at(8), msg->pose.covariance.at(12), msg->pose.covariance.at(13),
+        msg->pose.covariance.at(14);
+    cov_rot << msg->pose.covariance.at(21), msg->pose.covariance.at(22), msg->pose.covariance.at(23), msg->pose.covariance.at(27),
+        msg->pose.covariance.at(28), msg->pose.covariance.at(29), msg->pose.covariance.at(33), msg->pose.covariance.at(34),
+        msg->pose.covariance.at(35);
+    has_covariance = true;
+    write();
+  }
+
+  void callback_transform(const geometry_msgs::msg::TransformStamped::SharedPtr msg) {
+    timestamp = rclcpp::Time(msg->header.stamp).seconds();
+    q_ItoG << msg->transform.rotation.x, msg->transform.rotation.y, msg->transform.rotation.z, msg->transform.rotation.w;
+    p_IinG << msg->transform.translation.x, msg->transform.translation.y, msg->transform.translation.z;
+    write();
+  }
+#else
   void callback_odometry(const nav_msgs::OdometryPtr &msg) {
     timestamp = msg->header.stamp.toSec();
     q_ItoG << msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w;
@@ -103,10 +145,6 @@ public:
     write();
   }
 
-  /**
-   * @brief Callback for geometry_msgs::PoseStamped message types
-   * @param msg New message
-   */
   void callback_pose(const geometry_msgs::PoseStampedPtr &msg) {
     timestamp = msg->header.stamp.toSec();
     q_ItoG << msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z, msg->pose.orientation.w;
@@ -114,14 +152,6 @@ public:
     write();
   }
 
-  /**
-   * @brief Callback for geometry_msgs::PoseWithCovarianceStamped message types.
-   *
-   * Note that covariance is in the order (x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis).
-   * http://docs.ros.org/api/geometry_msgs/html/msg/PoseWithCovariance.html
-   *
-   * @param msg New message
-   */
   void callback_posecovariance(const geometry_msgs::PoseWithCovarianceStampedPtr &msg) {
     timestamp = msg->header.stamp.toSec();
     q_ItoG << msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w;
@@ -136,35 +166,23 @@ public:
     write();
   }
 
-  /**
-   * @brief Callback for geometry_msgs::TransformStamped message types
-   * @param msg New message
-   */
   void callback_transform(const geometry_msgs::TransformStampedPtr &msg) {
     timestamp = msg->header.stamp.toSec();
     q_ItoG << msg->transform.rotation.x, msg->transform.rotation.y, msg->transform.rotation.z, msg->transform.rotation.w;
     p_IinG << msg->transform.translation.x, msg->transform.translation.y, msg->transform.translation.z;
     write();
   }
+#endif
 
 protected:
-  /**
-   * @brief This is the main write function that will save to disk.
-   * This should be called after we have saved the desired pose to our class variables.
-   */
-  void write() {
 
-    // timestamp
+  void write() {
     outfile.precision(5);
     outfile.setf(std::ios::fixed, std::ios::floatfield);
     outfile << timestamp << " ";
-
-    // pose
     outfile.precision(6);
     outfile << p_IinG.x() << " " << p_IinG.y() << " " << p_IinG.z() << " " << q_ItoG(0) << " " << q_ItoG(1) << " " << q_ItoG(2) << " "
             << q_ItoG(3);
-
-    // output the covariance only if we have it
     if (has_covariance) {
       outfile.precision(10);
       outfile << " " << cov_rot(0, 0) << " " << cov_rot(0, 1) << " " << cov_rot(0, 2) << " " << cov_rot(1, 1) << " " << cov_rot(1, 2) << " "
@@ -175,10 +193,7 @@ protected:
     }
   }
 
-  // Output stream file
   std::ofstream outfile;
-
-  // Temp storage objects for our pose and its certainty
   bool has_covariance = false;
   double timestamp;
   Eigen::Vector4d q_ItoG;
